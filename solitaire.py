26548dd641e21d60e23f5035923fc7e080b0fc0a
Michael Rudow <mrudow@ftwdev.com>
2013-06-14 16:49:44 -0400

    cleaned up some errors

--- solitaire.py
+++ solitaire.py
@@ -9,14 +9,14 @@ class Deck:
     
 #mdc=move deck card
     self.deck[0]=card
-    def mdc(self, card, dest)
+    def mdc(self, card, dest):
 
       if dest==(stack0|stack1|stack2|stack3|stack4|stack5|stack6):
           if add_card_to_stack(dest, card)==1:
               add_card_to_stack(dest, card)
               del self.deck[0]
               i=1
-              for i<len(self.deck):
+              while i < len(self.deck):
                  self.deck[i-1] = self.deck[i]
                  del self.deck[i]
                  i=i+1
@@ -28,8 +28,8 @@ class Deck:
               add_card(dest, card)
               del self.deck[0]
               i=1
-              for i<len(self.deck):
-                 self.deck[i-1] = self.deck.[i]
+              while i<len(self.deck):
+                 self.deck[i-1] = self.deck[i]
                  del self.deck[i]
                  i=i+1
               self.ndc=self.ndc-1
@@ -37,11 +37,11 @@ class Deck:
               1==1
 
 #ndc=next deck card
-    def ndc(self, card)
+    def ndc(self, card):
       deck[len(deck)]=deck[0]
       del deck[0]
       i=0
-      for i<=len(deck)
+      while i<=len(deck):
         deck[i]=deck[i+1]
         del deck[i+1]
         i=i+1
@@ -65,13 +65,13 @@ class Stack:
     stack={}
     dstack={}
     
-    def flip_up(self, stack)
+    def flip_up(self, stack):
       if len(stack)==0 & len(dstack)>0:
           stack[0]=dstack[0]
           del dstack[0]
           i=1
-          for i<len(dstack):
-            dstack[i-1] = dstack.[i]
+          while i<len(dstack):
+            dstack[i-1] = dstack[i]
             del dstack[i]
             i=i+1
       else:
@@ -95,11 +95,11 @@ class Stack:
             if add_card_to_stack(self, next_stack, card)==1:
               i=0
               a=1
-              for i < len(stack):
-                  if stack[i]=card:
+              while i < len(stack):
+                  if stack[i]==card:
                     add_card_to_stack(self, next_stack, card)
                     a=a-1
-                    for a>0:
+                    while a>0:
                         add_card_to_stack(self, next_stack, stack[a])
                         a=a-1
                   else:
@@ -112,10 +112,10 @@ class Stack:
                 print ("operation is invalid")
 
 #m2cs=move to cstack
-    def m2cs(self, card, dest)
+    def m2cs(self, card, dest):
       if len(stack)==0:
           if len(dstack)==0:
-            print("there is no card to move"
+            print("there is no card to move")
           else:
             flip_up(dest)
             m2cs(self, card, dest)
@@ -132,8 +132,8 @@ class Stack:
             add_card(self, dest, card)
             del stack[0]
             i=1
-            for i<len(stack):
-              stack[i-1] = stack.[i]
+            while i<len(stack):
+              stack[i-1] = stack[i]
               del stack[i]
               i=i+1
         else:
@@ -167,11 +167,11 @@ class Cstack:
         if add_card_to_stack(self, dest, card)==1:
           add_card_to_stack(self, dest, card)
           del cstack[0]
-            i=1
-            for i<len(cstack):
-              cstack[i-1] = cstack.[i]
-              del cstack[i]
-              i=i+1
+          i=1
+          while i<len(cstack):
+            cstack[i-1] = cstack[i]
+            del cstack[i]
+            i=i+1
         else:
             print ("operation is against the rules")
 
@@ -186,18 +186,18 @@ class Cstack:
               print ("operation is invalid")
 
 
-
-stack0= Stack(0, 0)
-stack1= Stack(0, 1)
-stack2= Stack(0, 2)
-stack3= Stack(0, 3)
-stack4= Stack(0, 4)
-stack5= Stack(0, 5)
-stack6= Stack(0, 6)
-cstack0= Cstack()
-cstack1= Cstack()
-cstack2= Cstack()
-cstack3= Cstack()
+def new_game(self):
+    stack0= Stack(0, 0)
+    stack1= Stack(0, 1)
+    stack2= Stack(0, 2)
+    stack3= Stack(0, 3)
+    stack4= Stack(0, 4)
+    stack5= Stack(0, 5)
+    stack6= Stack(0, 6)
+    cstack0= Cstack()
+    cstack1= Cstack()
+    cstack2= Cstack()
+    cstack3= Cstack()
 
 
 #print(stack0, stack1, stack2, stack3, stack4, stack5, stack6, cstack0, cstack1, cstack2, cstack3, deck)
